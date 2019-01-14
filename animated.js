9d47b70570cecce7e0c524a254ad9a1f96473088
huangzhicheng <15659981870m@sina.cn>
2019-01-14 09:17:01 +0800

    feat:(初始化时也可使用)

--- animated.js
+++ animated.js
@@ -1,17 +1,17 @@
 export default {
   install (Vue) {
     Vue.directive('animated', {
-      bind: function (el , binding) {
-        let scroll = ()=>{
+      inserted: function (el , binding) {
+        let scroll = ()=> {
           let rect = el.getBoundingClientRect();
-          //判断进入可视区域
-          let isCheck = (typeof window !== 'undefined') && (rect.top < window.innerHeight && rect.bottom > 0) &&
+          let isCheck = window && (rect.top < window.innerHeight && rect.bottom > 0) &&
             (rect.left < window.innerWidth && rect.right > 0);
 
           if(isCheck && el.className.indexOf(binding.value) === -1){
             el.className += binding.value;
           }
-        };
+        }
+        scroll();
         window.addEventListener('scroll',scroll);
       }
     });
